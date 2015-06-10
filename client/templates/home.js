93e28a87a4421284f98c65a12b3db85c24ba7ac3
Ten Mutunhire <tmutunhire@gmail.com>
2015-06-10 17:04:41 +0000

    index blog posts

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,29 +1,19 @@
-Template.home.created = function () {
-	console.log('Created the home template');
-};
-
-Template.home.rendered = function() {
-	console.log('Rendered the home template');
-
-	this.$('p').html('We just replaced that text!');
-};
-
-Template.home.destroyed = function() {
-	console.log('Destroyed the home template');
-};
-
 Template.home.helpers({
-	exampleHelper: function() {
-		return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
-	},
-
-	dataContextHelper: function() {
-		return {
-			someText: 'This was set in the parent templates helper, as an argument.'
-			,someNested: {
-				text: 'That comes from "someNested.text"'
+	postsList: function() {
+		return [
+			{
+				title: 'My Second Entry',
+				description: 'Working hard in Kampala, hustling everyday.',
+				author: 'Tomahawk Tindo',
+				timeCreated: moment().subtract(3, 'days').unix()
+			},
+			{
+				title: 'My First Entry',
+				description: 'Life and Times of Tomahawk Ten',
+				author: 'Tomahawk Ten',
+				timeCreated: moment().subtract(7, 'days').unix()
 			}
-		};
+		];
 	}
 });
 
