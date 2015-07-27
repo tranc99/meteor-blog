66a32a96430feb983149797b8409d83aae054b0a
Ten Mutunhire <tmutunhire@gmail.com>
2015-07-27 10:12:35 +0000

    knowledge: ios, hybrid dev, meteor, flask, ember, rails api, android

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,7 +1,7 @@
-//Meteor.subscribe('all-posts');
-//Meteor.subscribe('specificfields-posts');
+// //Meteor.subscribe('all-posts');
+// //Meteor.subscribe('specificfields-posts');
 
-Session.setDefault('lazyloadLimit', 2);
-Tracker.autorun(function() {
-	Meteor.subscribe('lazyload-posts', Session.get('lazyloadLimit'));
-});
+// Session.setDefault('lazyloadLimit', 2);
+// Tracker.autorun(function() {
+// 	Meteor.subscribe('lazyload-posts', Session.get('lazyloadLimit'));
+// });
--- routes.js
+++ routes.js
@@ -1,3 +1,7 @@
+if(Meteor.isClient) {
+	Session.setDefault('lazyloadLimit', 1);
+}
+
 Router.configure({
 	layoutTemplate: 'layout',
 	notFoundTemplate: 'notFound'
@@ -7,8 +11,15 @@ Router.map(function() {
 
 	this.route('Home', {
 		path: '/',
-		template: 'home'
+		template: 'home',
+		subscriptions: function() {
+			return Meteor.subscribe("lazyload-posts", Session.get('lazyloadLimit'));
+		}
 	});
 
-	this.route('about');
+	//this.route('about');
+	this.route('About', {
+		path: '/about',
+		template: 'about'
+	});
 });
