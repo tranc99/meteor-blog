9c4e8ebc39d018cf8166e882b1b2797f7e29e0e3
Ten Mutunhire <tmutunhire@gmail.com>
2015-07-28 09:24:35 +0000

    now let us add allow and deny rules

---
+++ client/templates/editPost.js
@@ -0,0 +1,46 @@
+Template.editPost.events({
+	'submit form': function(e, tmpl) {
+		e.preventDefault();
+		console.log('Saving Post');
+		var form = e.target;
+		var user = Meteor.user();
+		_this = this;
+
+		//edit the post
+		if(this._id) {
+			Posts.update(this._id, {$set: {
+				title: 			form.title.value,
+				description: 	form.description.value,
+				text:  			form.text.value
+			}}, function(error) {
+				if(error) {
+					alert(error.reason);
+				} else {
+					Router.go('Post', {slug: _this.slug});
+				}
+			});
+		} else {
+			var slug = _.slugify(form.title.value);
+
+			// save the post in the collection
+			Posts.insert({
+				title: 			form.title.value,
+				slug: 			slug,
+				description: 	form.description.value,
+				text: 			form.text.value,
+				timeCreated:  	moment().unix(),
+				author: 		user.profile.name,
+				owner: 			user._id
+
+			}, function(error) {
+				if(error) {
+					//display the error to the user
+					alert(error.reason);
+				} else {
+					//redirect to the post
+					Router.go('Post', {slug: slug});
+				}
+			});
+		}
+	}
+});
